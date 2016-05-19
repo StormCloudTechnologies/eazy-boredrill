6db8531c7f8100546cbcf8169dacf5cd7d49b463
Sudarshan Vishwakarma <sudarshan.vis101@gmail.com>
2016-05-19 16:07:33 +0530

    Added admin panel UI.

---
+++ public/321889_786921/Mainjs/controllers/login.js
@@ -0,0 +1,5 @@
+angular.module('Login.Controllers', [])
+.controller('LoginCtrl', function($scope) {
+  
+
+})
---
+++ public/321889_786921/Mainjs/controllers/project.js
@@ -0,0 +1,5 @@
+angular.module('Project.Controllers', [])
+.controller('ProjectCtrl', function($scope) {
+  
+
+})
