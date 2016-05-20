ec485393d91c1594839cb688784d5e8dfbc0dfbc
irshadgo <irshad.gouri@hotwaxsystems.com>
2016-05-20 12:30:39 +0530

    Created API for projects and latest jobs.

--- server.js
+++ server.js
@@ -37,6 +37,16 @@ var Projects = mongoose.model('Projects', projectSchema);
 
 module.exports = Projects;
 
+var latestJobsSchema = new Schema({
+    project_name: String,
+    project_description: String,
+    images: [String]
+});
+
+var LatestJobs = mongoose.model('LatestJobs', latestJobsSchema);
+
+module.exports = LatestJobs;
+
 // listen (start app with node server.js) ======================================
 app.listen(8000);
 console.log("App listening on port 8000");
@@ -83,4 +93,164 @@ app.post('/api/sendMail', function(req, res) {
         }
         return res.json({"message":"Message sent successfully."});
       });
+});
+
+/** API for sending mail to admin */
+app.post('/api/forgotAdminPassword', function(req, res) {
+   var msg = {
+      html: "<b>Hello!</b><p>Your password for Eazy Boredrill account <strong>eazyboredrill</strong> is <strong>eazyboredrilladmin1</strong></p>.",
+      createTextFromHtml: true,
+      from:    "<eazybiz.biz@gmail.com>",
+      to: "<eazybiz.biz@gmail.com>",
+      subject: "Eazy Boredrill Credentials"
+    };
+    transport.sendMail(msg, function (err) {
+      if (err) {
+        return;
+      }
+
+
+      return res.json({"successMessage":"Credentials has been sent to your Email."});
+    });
+
+});
+
+// Projects API
+
+// add project
+app.post('/api/addProject', function(req, res) {
+    var delete_images = req.body.delete_images;
+
+    Projects.create(req.body.projectData, function(err, projects) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        if(projects) {
+            return res.json({"message":"Project has been added successfully."});
+        }
+    });
+    if(delete_images.length > 0) {
+        delete_images.forEach(function(file_path) {
+          fs.unlink(file_path);
+        });
+    }
+
+});
+
+// update project
+app.put('/api/updateProject', function(req, res) {
+    var delete_images = req.body.delete_images;
+    Projects.findByIdAndUpdate(req.body.projectData._id, req.body.projectData
+    , function(err, projects) {
+        if (err)
+            res.send(err);
+        if(delete_images.length > 0) {
+            delete_images.forEach(function(file_path) {
+              fs.unlink(file_path);
+            });
+        }
+        return res.json({"message":"Updated successfully."});
+    });
+});
+
+// delete project
+app.delete('/api/removeProject', function(req, res) {
+    var delete_images = req.body.images;
+    Projects.remove({
+        _id : req.body._id
+    }, function(err, project) {
+        if (err)
+            res.send(err);
+        if(delete_images.length > 0) {
+            delete_images.forEach(function(file_path) {
+              fs.unlink(file_path);
+            });
+        }
+        Projects.find(function(err, projects) {
+            if (err)
+                res.send(err)
+            res.json(projects); // return all advertisement in JSON format
+        });
+    });
+});
+
+// get projects
+app.post('/api/getProjects', function(req, res) {
+    Projects.find(req.body.projectData, function(err, projects) {
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+        res.json(projects); // return all advertisement in JSON format
+    });
+});
+
+// Latest Jobs API
+
+// add latestJob
+app.post('/api/addLatestJob', function(req, res) {
+    var delete_images = req.body.delete_images;
+
+    LatestJobs.create(req.body.latestJobData, function(err, latestJob) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        if(latestJob) {
+            return res.json({"message":"Project has been added successfully."});
+        }
+    });
+    if(delete_images.length > 0) {
+        delete_images.forEach(function(file_path) {
+          fs.unlink(file_path);
+        });
+    }
+
+});
+
+// update latestJob
+app.put('/api/updateLatestJob', function(req, res) {
+    var delete_images = req.body.delete_images;
+    LatestJobs.findByIdAndUpdate(req.body.latestJobData._id, req.body.latestJobData
+    , function(err, latestJob) {
+        if (err)
+            res.send(err);
+        if(delete_images.length > 0) {
+            delete_images.forEach(function(file_path) {
+              fs.unlink(file_path);
+            });
+        }
+        return res.json({"message":"Updated successfully."});
+    });
+});
+
+// delete latestJob
+app.delete('/api/removeLatestJob', function(req, res) {
+    var delete_images = req.body.images;
+    LatestJobs.remove({
+        _id : req.body._id
+    }, function(err, latestJob) {
+        if (err)
+            res.send(err);
+        if(delete_images.length > 0) {
+            delete_images.forEach(function(file_path) {
+              fs.unlink(file_path);
+            });
+        }
+        LatestJobs.find(function(err, latestJobs) {
+            if (err)
+                res.send(err)
+            res.json(latestJobs); // return all advertisement in JSON format
+        });
+    });
+});
+
+// get latestJobs
+app.post('/api/getLatestJobs', function(req, res) {
+    LatestJobs.find(req.body.latestJobData, function(err, latestJobs) {
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+        res.json(latestJobs); // return all advertisement in JSON format
+    });
 });
