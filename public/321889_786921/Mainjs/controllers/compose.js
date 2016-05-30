13405e66518fe839e2ed6f17046805a2265f2d8f
Sudarshan Vishwakarma <sudarshan.vis101@gmail.com>
2016-05-30 15:58:47 +0530

    compose editer changes

--- public/321889_786921/Mainjs/controllers/compose.js
+++ public/321889_786921/Mainjs/controllers/compose.js
@@ -1,4 +1,5 @@
-angular.module('starter', ['APIModule', 'ngFileUpload', 'ui.bootstrap'])
+var url = 'http://localhost:8000/';
+angular.module('starter', ['APIModule', 'ngFileUpload', 'ui.bootstrap', 'colorpicker.module', 'wysiwyg.module'])
 .factory('$localstorage', ['$window', function($window) {
   return {
     set: function(key, value) {
@@ -52,16 +53,46 @@ angular.module('starter', ['APIModule', 'ngFileUpload', 'ui.bootstrap'])
 	  if(islogin!=1){
 	      window.location = "index.html";
 	  }
-  
+    console.log("hiii");
 	$scope.logout = function(){
        $localstorage.set('islogin', "0");
 	   window.location = "index.html";
 	}
-    $scope.compose = {};
-    $scope.compose.editerdata = "";
+    $scope.compose = {images:[]};
+    $scope.inputMessage = "";
     $scope.editerdata = function(){
-        console.log($scope.compose.editerdata);
+      console.log($scope.inputMessage);
     }
+
+    $scope.deleteImages = [];
+    $scope.removeChoice = function(index){
+        $scope.deleteImages.push($scope.compose.images[index]);
+        $scope.compose.images.splice(index,1);
+    };
+    $scope.ImagePath = [];  
+    $scope.uploadPhoto = function(file) {
+        file.upload = Upload.upload({
+          url: url + 'api/uploadPhotos',
+          arrayKey: '',
+          data: {file: file}
+        });
+
+        file.upload.then(function (response) {
+           console.log(response);
+           if(response.data.length > 0) {
+                angular.forEach(response.data, function(item){
+                   $scope.compose.images.push(item.path);
+                   // $scope.project.images = $scope.ImagePath;
+                });
+             }
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+    };
 
 
 })
