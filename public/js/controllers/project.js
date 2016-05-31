299f951a43bed43b6ee0d82e58446715bb4dd1a9
Sudarshan Vishwakarma <sudarshan.vis101@gmail.com>
2016-05-31 16:03:01 +0530

    loader website project file

--- public/js/controllers/project.js
+++ public/js/controllers/project.js
@@ -1,4 +1,37 @@
 angular.module('starter', ['APIModule', 'ui.bootstrap'])
+.factory('genericInterceptor', function($q, $rootScope) {
+    var interceptor = {
+        'request': function(config) {
+            // Successful request method
+            $rootScope.loadCompetition = true;
+            return config; // or $q.when(config);
+        },
+        'response': function(response) {
+            // Successful response
+            $rootScope.loadCompetition = false;
+            return response; // or $q.when(config);
+        },
+        'requestError': function(rejection) {
+            // An error happened on the request
+            // if we can recover from the error
+            // we can return a new request
+            // or promise
+            $rootScope.loadCompetition = false;
+            return response;
+            // Otherwise, we can reject the next
+            // by returning a rejection
+            // return $q.reject(rejection);
+        },
+        'responseError': function(rejection) {
+            
+            // Returning a rejection
+            $rootScope.loadCompetition = false;
+            return rejection;
+        }
+    };
+    return interceptor;
+})
+
 .controller('ProjectCtrl', function($scope, APIService, $uibModal) {
   
   $scope.ProjectLists = [];
